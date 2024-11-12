b03d6116d142fe0e4ab4d5d07527b82370bfc394
Tatsuya Namikwa <m757e888a7n7r@gmaiil.com>
2024-11-12 23:34:57 +0900

    er図表示できるようになった

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>{{ text }}</p>
+  <v-btn @click="submit" variant="outlined"> Button </v-btn>
+  <ul v-for="user in users">
+    <li>{{ user.name }}</li>
+    <li>{{ user.email }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const text = ref("");
+const submit = () => {
+  text.value = "text2";
+};
+</script>
+
+<style lang="scss" scope>
+v-btn {
+  border: solid 1px;
+  padding: 5px;
+  &:hover {
+    opacity: 0.5;
+    cursor: pointer;
+  }
+}
+</style>
